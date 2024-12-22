2ba6c4d8243ca221c461e5527f0b14c1f1c48d91
newnewminsuk <rhalstjr106@gmail.com>
2024-12-22 15:14:04 +0900

    첫번째 커밋

---
+++ src/main/java/org/example/xmas_project/XmasProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.xmas_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmasProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XmasProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/xmas_project/XmasProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.xmas_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmasProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
